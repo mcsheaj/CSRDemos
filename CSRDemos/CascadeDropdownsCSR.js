b7e2511500d1d77b42ef079262e766cf723deed6
mcsheaj <golf2day>
2016-08-04 22:39:54 -0400

    sps pittsburgh

---
+++ CSRDemos/CascadeDropdownsCSR.js
@@ -0,0 +1,57 @@
+(function($) {
+    // this structure is the only thing that needs to be modified to override more or different fields
+    var fields = {
+        "SalesDivision": {
+            "parent": "SalesRegion",
+            "child": "SalesDivision",
+            "relationshipList": "SalesDivision",
+            "relationshipParent": "SalesRegion",
+            "relationshipChild": "Title"
+        },
+        "SalesState": {
+            "parent": "SalesDivision",
+            "child": "SalesState",
+            "relationshipList": "SalesState",
+            "relationshipParent": "SalesDivision",
+            "relationshipChild": "Title"
+        }
+    };
+
+    var keys = Object.keys(fields);
+
+    /*
+     * Create an empty overrides object.
+     */
+    var overrides = {
+        OnPostRender: function(ctx) {
+            if ($.inArray(ctx.ListSchema.Field[0].Name, keys) > -1) {
+                var config = fields[ctx.ListSchema.Field[0].Name];
+
+                var options = {
+                    parentColumn: config.parent,
+                    childColumn: config.child,
+                    relationshipList: config.relationshipList,
+                    relationshipListParentColumn: config.relationshipParent,
+                    relationshipListChildColumn: config.relationshipChild,
+                    debug: true
+                };
+
+                $().SPServices.SPCascadeDropdowns(options);
+            }
+        }
+    };
+
+    // check for MDS
+    if (typeof _spPageContextInfo != 'undefined' && _spPageContextInfo != null) {
+        // MDS is enabled
+        var url = (_spPageContextInfo.siteServerRelativeUrl === '/' ? "" : _spPageContextInfo.siteServerRelativeUrl) +
+            '/Style%20Library/AutocompleteCSR.js';
+        // register a callback to register the templates on partial page loads
+        RegisterModuleInit(url.toLowerCase(), function() {
+            SPClientTemplates.TemplateManager.RegisterTemplateOverrides(overrides);
+        });
+    }
+
+    // also just register for full page loads (F5/refresh)
+    SPClientTemplates.TemplateManager.RegisterTemplateOverrides(overrides);
+})(jQuery);
